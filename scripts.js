64142dc8d733e6ed9745dde617b5ce5eb0414041
ADRIANO NAVARRO <Adriano_navarro@outlook.com>
2023-11-18 10:35:30 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,28 @@
+// Define a function that gets the list of open tabs
+async function getTabs() {
+  try {
+    // Fetch the configuration from the Express server
+    let response = await fetch("http://localhost:3000/blocker");
+    let data = await response.json();
+
+    if (data.blocked) {
+      chrome.tabs.query({}, function (tabs) {
+        tabs.forEach((tab) => {
+          let shouldClose = data.websites.every(allowedUrl => !tab.url.includes(allowedUrl));
+
+          if (shouldClose) {
+            chrome.tabs.remove(tab.id);
+          }
+        });
+      });
+    }
+  } catch (error) {
+    console.error('Error fetching blocker configuration:', error);
+  }
+}
+
+// Define a variable for the interval in milliseconds
+var interval = 5000; // 5 seconds
+
+// Use the setInterval function to call the getTabs function every N seconds
+setInterval(getTabs, interval);
